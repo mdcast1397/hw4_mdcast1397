cd04f36c23ea067771e4833ba47b298cf87e1166
Michael  Castaneda <michael.castaneda@kellogg.northwestern.edu>
2025-03-18 02:40:12 +0000

    Completed setup, ran migrations, installed gems

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_013536) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
